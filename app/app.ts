4a366c3c65cbc5e86c80c71933af242d3ba87510
Dimitar Hristov <dimitar.hristov@fluke.com>
2020-02-22 12:05:19 +0100

    initial commit

---
+++ app/app.ts
@@ -0,0 +1,11 @@
+import express = require("express");
+
+const app: express.Application = express();
+
+app.get("/", function(req, res) {
+  res.send("Hello World!");
+});
+
+app.listen(9999, function() {
+  console.log("listening on port 9999!");
+});
