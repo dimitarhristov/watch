f2ccf3a11bdd9913c3a4eef8b28e638ff665e138
Dimitar Hristov <dimitar.hristov@fluke.com>
2020-02-22 21:44:20 +0100

    more changes in config

---
+++ server/index.ts
@@ -0,0 +1,23 @@
+import express = require("express");
+const app = express();
+import cors = require("cors");
+import bodyParser = require("body-parser");
+import logger = require("morgan");
+
+const port = process.env.PORT || 9999;
+
+app.use(logger("dev"));
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+import dashboardRouter from "./router/dashboard";
+app.use("/dashboard", dashboardRouter);
+
+app.listen(port, function() {
+  console.log("Runnning on " + port);
+});
+
+module.exports = app;
---
+++ server/router/dashboard.ts
@@ -0,0 +1,18 @@
+import express = require("express");
+const router = express.Router();
+import { players } from "../db/dummyDatabase";
+
+router.get("/list", async (req, res) => {
+  try {
+    res.status(200).json({
+      data: players
+    });
+  } catch (err) {
+    res.status(400).json({
+      message: "Some error occured",
+      err
+    });
+  }
+});
+
+export default router;
